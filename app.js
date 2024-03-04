3dfb56b50478df20eb8c260aa3230f6eca2528c3
Brendan McGinty <brendan.mcginty14@gmail.com>
2024-03-04 10:43:00 -0500

    expanded the addRole and addEmployee functions to ensure they prompt for all needed inputs

--- app.js
+++ app.js
@@ -124,38 +124,73 @@ function addDepartment() {
 // implement functions to add roles, employees, and update employee roles
 
 function addRole() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Enter the name of the role:'
-        }
-    ]).then(answer => {
-        // Execute SQL query to add department
-        connection.query('INSERT INTO role SET ?', { name: answer.name }, (err, results) => {
+  inquirer.prompt([
+      {
+          type: 'input',
+          name: 'title',
+          message: 'Enter the title of the role:'
+      },
+      {
+          type: 'input',
+          name: 'salary',
+          message: 'Enter the salary for this role:'
+      },
+      {
+          type: 'input',
+          name: 'departmentId',
+          message: 'Enter the department ID for this role:'
+      }
+  ]).then(answer => {
+      // Execute SQL query to add role
+      connection.query('INSERT INTO role SET ?', {
+          title: answer.title,
+          salary: answer.salary,
+          department_id: answer.departmentId
+      }, (err, results) => {
           if (err) throw err;
           console.log('Role added successfully.');
           startApp(); // Go back to main menu
-        });
       });
+  });
 }
 
 function addEmployee() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Enter the name of the new employee:'
-        }
-    ]).then(answer => {
-        // Execute SQL query to add department
-        connection.query('INSERT INTO employee SET ?', { name: answer.name }, (err, results) => {
+  inquirer.prompt([
+      {
+          type: 'input',
+          name: 'firstName',
+          message: 'Enter the first name of the new employee:'
+      },
+      {
+          type: 'input',
+          name: 'lastName',
+          message: 'Enter the last name of the new employee:'
+      },
+      {
+          type: 'input',
+          name: 'roleId',
+          message: 'Enter the role ID for the new employee:'
+      },
+      {
+          type: 'input',
+          name: 'managerId',
+          message: 'Enter the manager ID for the new employee:'
+      }
+  ]).then(answer => {
+      // Execute SQL query to add employee
+      connection.query('INSERT INTO employee SET ?', {
+          first_name: answer.firstName,
+          last_name: answer.lastName,
+          role_id: answer.roleId,
+          manager_id: answer.managerId
+      }, (err, results) => {
           if (err) throw err;
           console.log('Employee added successfully.');
           startApp(); // Go back to main menu
-        });
       });
+  });
 }
+
 
 function updateEmployeeRole() {
     // Logic to prompt the user for the employee and their new role
